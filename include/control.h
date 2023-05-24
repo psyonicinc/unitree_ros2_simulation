a29773f6b92e5cd0b52bb44d2a92a713dec8f0b9
aihoque2 <aihoque2@illinois.edu>
2023-05-24 01:15:30 -0700

    trying this again

---
+++ include/control.h
@@ -0,0 +1,6 @@
+#include "ros/ros.h"
+#include "unitree_legged_msgs/LowCmd.h"
+#include "unitree_legged_msgs/LowState.h"
+#include "unitree_legged_msgs/HighState.h"
+#define PosStopF (2.146E+9f)
+#define VelStopF (16000.f)
