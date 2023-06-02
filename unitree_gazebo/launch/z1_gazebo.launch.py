0a06dba9a5363b4a592b227bcaed1a5168d6a1f6
aihoque2 <aihoque2@illinois.edu>
2023-06-01 22:31:38 -0700

    gazebo z1 effort set up

--- unitree_gazebo/launch/z1_gazebo.launch.py
+++ unitree_gazebo/launch/z1_gazebo.launch.py
@@ -50,6 +50,18 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration('gui'))
     )
 
+    load_joint_state_broadcaster = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_effort_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active', 'joint_effort_controller'],
+        output='screen'
+    )
+
+
     # Gazebo launch
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -69,6 +81,18 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(name='gui', default_value='True',
                                             description='Flag to enable joint_state_publisher_gui'),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=spawn_entity,
+                on_exit=[load_joint_state_broadcaster],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_broadcaster,
+                on_exit=[load_joint_effort_controller],
+            )
+        ),
         gazebo,
         model_arg,
         joint_state_publisher_node,
