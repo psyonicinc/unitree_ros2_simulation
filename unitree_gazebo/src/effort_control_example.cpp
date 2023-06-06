17f12694d2fcc62ebad2675acb543688d0465225
aihoque2 <aihoque2@illinois.edu>
2023-06-05 22:18:43 -0700

    effort example control is working

--- unitree_gazebo/src/effort_control_example.cpp
+++ unitree_gazebo/src/effort_control_example.cpp
@@ -10,26 +10,26 @@ class EffortExample: public rclcpp::Node{
     public:
         EffortExample() : Node("effort_control_example") 
         {
-            pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("/joint_effort_controller/commands", 10);   
+            pub_ = this->create_publisher<std_msgs::msg::Float64MultiArray>("joint_effort_controller/commands", 10);   
             timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&EffortExample::timer_callback, this));
             force_flag_ = true;
             rest_ = std::vector<double>(6, 0.0);
-            force_ = std::vector<double>(6, 2.5);
-            msg_ = std::make_shared<std_msgs::msg::Float64MultiArray>();
+            force_ = {0.0, 0.0, -10.5, 0.0, 0.0, 0.0};
+            
         }
 
         void timer_callback(){
-            if (force_flag_) msg_->data = force_;
-            else msg_->data = rest_;
+            std_msgs::msg::Float64MultiArray msg_ = std_msgs::msg::Float64MultiArray();
+            if (force_flag_) msg_.data = force_;
+            else msg_.data = rest_;
 
-            pub_->publish(*msg_);
+            pub_->publish(msg_);
             force_flag_ = !force_flag_; 
         }
 
     private:
         std::vector<double> rest_;
         std::vector<double> force_;
-        std::shared_ptr<std_msgs::msg::Float64MultiArray> msg_;
         bool force_flag_;
         rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr pub_;
         rclcpp::TimerBase::SharedPtr timer_;
